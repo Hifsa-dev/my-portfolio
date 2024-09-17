6134fbf797eae7a62c3937dc962abc3395205609
Hifsa-dev <hifsaafzal31@gmail.com>
2024-09-17 21:19:30 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
+// Assuming that solarSystem and sun elements are available in the DOM
+const solarSystem = document.querySelector('.solar-system');
 
 const fixIt = () => {
-	solarSystem.style.display = "none";
-	requestAnimationFrame(() => {
-		solarSystem.style.display = "";
-	});
-}
+    // Temporarily hide and show the solarSystem to reset the layout
+    solarSystem.style.display = "none";
+    requestAnimationFrame(() => {
+        solarSystem.style.display = "";
+    });
+};
 
 const ro = new ResizeObserver(entries => {
-	const sunHeight = solarSystem.querySelector(".sun").getBoundingClientRect().height;
-	const solarSystemHeight = solarSystem.getBoundingClientRect().height;
-	if(solarSystemHeight > 10 * sunHeight) {
-		fixIt();
-	}
+    // Get the height of the sun and the solarSystem element
+    const sun = solarSystem.querySelector(".sun");
+    if (sun) {
+        const sunHeight = sun.getBoundingClientRect().height;
+        const solarSystemHeight = solarSystem.getBoundingClientRect().height;
+
+        // If the solar system's height exceeds 10 times the sun's height, fix the layout
+        if (solarSystemHeight > 10 * sunHeight) {
+            fixIt();
+        }
+    }
 });
+
+// Start observing the solarSystem element for resizing
+ro.observe(solarSystem);
